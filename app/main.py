01dd6d70637c15b6c80e24646acc82e4cbf4bff4
nekitoseek <nmamaev_04@mail.ru>
2025-04-28 22:05:15 +0700

    Почистил main

--- app/main.py
+++ app/main.py
@@ -1,48 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from app import crud, schemas
-from app.database import get_db
-from app.schemas import QueueCreate, QueueOut, UserOut, NotificationOut
-from app.crud import create_queue, get_queues
-from app import auth
-from app.schemas import UserLogin, Token
-from app.models import User, Notification
-from app.auth import get_current_user
+from app import auth, crud, models, schemas, database
 from typing import List, Optional
 
 app = FastAPI()
-
-# Разрешаем доступ с фронта
-origins = ["http://localhost:5173"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 async def root():
     return {"message": "Backend работает!"}
 
-
-# регистрация
+# api регистрации
 @app.post("/register", response_model=schemas.UserOut)
-async def register_user(user: schemas.UserCreate, db: AsyncSession = Depends(get_db)):
+async def register_user(user: schemas.UserCreate, db: AsyncSession = Depends(database.get_db)):
     return await crud.create_user(db, user)
 
-from fastapi.security import OAuth2PasswordRequestForm
-
-
-# авторизация
-@app.post("/login", response_model=Token)
+# api авторизации
+@app.post("/login", response_model=schemas.Token)
 async def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
-    db: AsyncSession = Depends(get_db)
+    db: AsyncSession = Depends(database.get_db)
 ):
     db_user = await auth.authenticate_user(db, form_data.username, form_data.password)
     if not db_user:
@@ -51,136 +30,139 @@ async def login(
     access_token = auth.create_access_token(data={"sub": str(db_user.id)})
     return {"access_token": access_token, "token_type": "bearer"}
 
-
-# проверка роли пользователя
+# api для проверки роли пользователя
 @app.get("/me")
-async def read_current_user(current_user: User = Depends(get_current_user)):
+async def read_current_user(current_user: models.User = Depends(auth.get_current_user)):
     return {"username": current_user.username, "role": current_user.role}
 
-
-# создание очереди
-@app.post("/queues", response_model=QueueOut)
+# api создания очереди
+@app.post("/queues", response_model=schemas.QueueOut)
 async def create_queue_route(
-        queue: QueueCreate,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user)
+        queue: schemas.QueueCreate,
+        db: AsyncSession = Depends(database.get_db),
+        current_user: models.User = Depends(auth.get_current_user)
 ):
     if current_user.role != "teacher":
         raise HTTPException(status_code=403, detail="Только преподаватели могут создавать очереди")
-    return await create_queue(db, queue, teacher_id=current_user.id)
+    return await crud.create_queue(db, queue, teacher_id=current_user.id)
 
-
-# просмотр активных очередей
-@app.get("/queues", response_model=List[QueueOut])
+# api просмотр активных очередей
+@app.get("/queues", response_model=List[schemas.QueueOut])
 async def get_queues_route(
     group_id: Optional[int] = None,
     discipline_id: Optional[int] = None,
     status: str = "active",
-    db: AsyncSession = Depends(get_db),
-    current_user: User = Depends(get_current_user)
+    db: AsyncSession = Depends(database.get_db),
+    current_user: models.User = Depends(auth.get_current_user)
 ):
-    return await get_queues(db, group_id, discipline_id, status)
+    return await crud.get_queues(db, group_id, discipline_id, status)
 
-# удаление очереди (только владелец)
+# api для удаления очереди (только владелец)
 @app.delete("/queues/{queue_id}")
 async def delete_queue_route(
         queue_id: int,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user)
+        db: AsyncSession = Depends(database.get_db),
+        current_user: models.User = Depends(auth.get_current_user)
 ):
     return await crud.delete_queue(db, queue_id, current_user)
 
-
-# редактирование очереди (только владелец)
-@app.patch("/queues/{queue_id}", response_model=QueueOut)
+# api для редактирования очереди (только владелец)
+@app.patch("/queues/{queue_id}", response_model=schemas.QueueOut)
 async def update_queue_route(
         queue_id: int,
         queue_data: schemas.QueueUpdate,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user)
+        db: AsyncSession = Depends(database.get_db),
+        current_user: models.User = Depends(auth.get_current_user)
 ):
     return await crud.queue_update(db, queue_id, queue_data, current_user)
 
 
-# просмотр студентов в очереди
-@app.get("/queues/{queue_id}/students", response_model=List[UserOut])
+# api для просмотра студентов в очереди
+@app.get("/queues/{queue_id}/students", response_model=List[schemas.UserOut])
 async def get_queue_students(
         queue_id: int,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user)
+        db: AsyncSession = Depends(database.get_db),
+        current_user: models.User = Depends(auth.get_current_user)
 ):
     return await crud.get_students_in_queue(db, queue_id)
 
 
-# присоединение к очереди
+# api для присоединения к очереди
 @app.post("/queues/{queue_id}/join")
 async def join_queue_route(
         queue_id: int,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user)
+        db: AsyncSession = Depends(database.get_db),
+        current_user: models.User = Depends(auth.get_current_user)
 ):
     if current_user.role != "student":
         raise HTTPException(status_code=403, detail="Только студенты могут вставать в очереди")
     return await crud.join_queue(db, queue_id, current_user.id)
 
-
-# покидание очереди
+# api для покидания очереди
 @app.post("/queues/{queue_id}/leave")
 async def leave_queue_route(
         queue_id: int,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user)
+        db: AsyncSession = Depends(database.get_db),
+        current_user: models.User = Depends(auth.get_current_user)
 ):
     if current_user.role != "student":
         raise HTTPException(status_code=403, detail="Только студенты могут покидать очередь")
 
     return await crud.leave_queue(db, queue_id, current_user.id)
 
-
-# вызов следующего
+# api для вызова следующего
 @app.post("/queues/{queue_id}/call")
 async def call_next_student_route(
         queue_id: int,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user)
+        db: AsyncSession = Depends(database.get_db),
+        current_user: models.User = Depends(auth.get_current_user)
 ):
     if current_user.role != "teacher":
         raise HTTPException(status_code=403, detail="Только преподаватели могут вызывать студентов")
     return await crud.call_next_student(db, queue_id, current_user.id)
 
-
-# завершение очереди
+# api для закрытия очереди
 @app.post("/queues/{queue_id}/close")
 async def close_queue_route(
         queue_id: int,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user)
+        db: AsyncSession = Depends(database.get_db),
+        current_user: models.User = Depends(auth.get_current_user)
 ):
     if current_user.role != "teacher":
         raise HTTPException(status_code=403, detail="Только преподаватели могут закрывать очередь")
 
     return await crud.close_queue(db, queue_id, current_user.id)
 
-
-# завершение сдачи
+# api для завершения сдачи (конкретный студент)
 @app.post("/queues/{queue_id}/complete")
 async def complete_student_route(
         queue_id: int,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user)
+        db: AsyncSession = Depends(database.get_db),
+        current_user: models.User = Depends(auth.get_current_user)
 ):
     if current_user.role != "teacher":
         raise HTTPException(status_code=403, detail="Только преподаватели могут завершать вызов")
     return await crud.complete_current_student(db, queue_id, current_user.id)
 
-
-# уведомления
-@app.get("/notifications", response_model=List[NotificationOut])
+# api для уведомлений (просто с бд пока что)
+@app.get("/notifications", response_model=List[schemas.NotificationOut])
 async def get_my_notifications(
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user)
+        db: AsyncSession = Depends(database.get_db),
+        current_user: models.User = Depends(auth.get_current_user)
 ):
     result = await db.execute(
-        select(Notification).where(Notification.user_id == current_user.id)
+        select(models.Notification).where(models.Notification.user_id == current_user.id)
     )
     return result.scalars().all()
+
+
+
+# Разрешаем доступ с фронта
+# origins = ["http://localhost:5173"]
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
